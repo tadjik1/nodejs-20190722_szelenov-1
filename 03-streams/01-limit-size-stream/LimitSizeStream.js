07cbedf438fdae2e2018c7315ba7c34dec11e212
Sergey Zelenov <mail@zelenov.su>
2020-05-09 11:57:09 +0200

    3/1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,15 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size += chunk.length;
+    if (this.size > this.limit) return callback(new LimitExceededError());
+    return callback(null, chunk);
   }
 }
 
